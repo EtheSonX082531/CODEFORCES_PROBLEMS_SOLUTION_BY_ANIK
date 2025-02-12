87eff106a726f5167224fdc45b92eefad3af285b
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2025-02-12 21:14:02 +0600

    Create A. Create A New String.cpp

---
+++ A. Create A New String.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s,t,k;
+    cin>>s>>t;
+    cout<<s.length()<<" "<<t.length()<<endl;
+    k=s+" "+t;
+    cout<<k<<endl;
+    return 0;
+}
+//https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/A
