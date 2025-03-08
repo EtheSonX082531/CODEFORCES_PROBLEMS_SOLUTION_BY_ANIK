90b112b299a6e00bc8818866d84115e08a2d5157
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2025-03-08 12:45:33 +0600

    Create A. Toy Army.cpp

---
+++ A. Toy Army.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<(3*n)/2;
+    return 0;
+}
+//https://codeforces.com/problemset/problem/84/A?fbclid=PAY2xjawI4xs9leHRuA2FlbQIxMAABpvpPYTD31t2sjOFz-166_INqJfDwoj3kTuyLO7UanAugBG0UAnO-qETUiw_aem_7FYMihOFSKj7yOgTcIMtaQ
