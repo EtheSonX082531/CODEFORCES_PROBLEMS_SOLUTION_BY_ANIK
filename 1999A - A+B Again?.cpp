0ab1150d91bae4829a8f769d79c46c97467f4752
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2025-06-08 19:00:48 +0600

    Create 1999A - A+B Again?.cpp

---
+++ 1999A - A+B Again?.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) {
+        int a;
+        cin>>a;
+        int sum=(a%10)+(a/10);
+        cout<<sum<<endl;
+    }
+    return 0;
+}
+//https://codeforces.com/problemset/problem/1999/A
